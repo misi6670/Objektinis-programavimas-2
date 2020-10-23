2a712ced6284ac687692f3eed572fea96fbc3553
misi6670 <55701043+misi6670@users.noreply.github.com>
2020-10-23 11:26:35 +0300

    Update and rename v0.4.cpp to v0.5.cpp

--- v0.5.cpp
+++ v0.5.cpp
@@ -1,4 +1,4 @@
-// v0.4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// v0.5.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "header.h"
