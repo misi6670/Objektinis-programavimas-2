ed6082037ec5430e53c9e171a4b10379524f7e22
misi6670 <55701043+misi6670@users.noreply.github.com>
2021-01-06 17:52:46 +0200

    Add files via upload

--- studentas.h
+++ studentas.h
@@ -54,8 +54,39 @@ public:
         galutinis_ = 0;
     }
     ~Studentas() { nd_.clear(); }
-    Studentas(const Studentas& stud);
-    Studentas& operator=(const Studentas& stud);
+    Studentas(const Studentas& stud)
+        : Zmogus(stud.Vardas_, stud.Pavarde_),
+        nd_{ stud.nd_ },
+        n_{ stud.n_ },
+        egz_{ stud.egz_ },
+        galutinis_{ stud.galutinis_ },
+        vid_{ stud.vid_ },
+        med_{ stud.med_ },
+        vidarmed_{ stud.vidarmed_ } {
+        Vardas_ = stud.Vardas_;
+        Pavarde_ = stud.Pavarde_;
+        nd_ = stud.nd_;
+        n_ = stud.n_;
+        egz_ = stud.egz_;
+        galutinis_ = stud.galutinis_;
+        vid_ = stud.vid_;
+        med_ = stud.med_;
+        vidarmed_ = stud.vidarmed_;
+    }
+    Studentas& operator=(const Studentas& stud) {
+        if (&stud == this) return *this;
+        nd_.clear();
+        Vardas_ = stud.Vardas_;
+        Pavarde_ = stud.Pavarde_;
+        nd_ = stud.nd_;
+        n_ = stud.n_;
+        egz_ = stud.egz_;
+        galutinis_ = stud.galutinis_;
+        vid_ = stud.vid_;
+        med_ = stud.med_;
+        vidarmed_ = stud.vidarmed_;
+        return *this;
+    }
     void setVidarmed(int vam) { vidarmed_ = vam; }
     int getVidarmed() const { return vidarmed_; }
     void setND(vector <int> nd) { nd_ = nd; nd_.reserve(n_); vid_ = countVidurkis(); med_ = countMediana(); }
@@ -66,9 +97,62 @@ public:
     int getEgzaminas() const { return egz_; }
     void setGalutinis() { if (vidarmed_ == 1) galutinis_ = countGalutinis(med_); else galutinis_ = countGalutinis(vid_); }
     float getGalutinis() const { return galutinis_; }
-    float countVidurkis();
-    float countMediana();
-    float countGalutinis(float vam);
-    bool operator <(const Studentas& a) const;
-    void isvedimas(char separator, int VardSimb, int PavSimb, int GalutSimb);
+    float countVidurkis() {
+        float suma = 0;
+        for (float a : nd_) suma = suma + a;
+        return vid_ = suma / n_;
+    }
+    float countMediana() {
+        sort(nd_.begin(), nd_.end());
+        if (n_ % 2 != 0) return med_ = (float)nd_.at(n_ / 2);
+        else {
+            int m;
+            m = nd_.at((n_ - 1) / 2) + nd_.at(n_ / 2);
+            return med_ = (float)m / 2.0;
+        }
+    }
+    float countGalutinis(float vam) {
+        return galutinis_ = vam * 0.4 + (float)egz_ * 0.6;
+    }
+    bool operator <(const Studentas& a) const {
+        if (getVardas() != a.getVardas())
+            return getVardas().compare(a.getVardas()) < 0;
+
+        if (getPavarde() != a.getPavarde())
+            return getPavarde().compare(a.getPavarde()) < 0;
+
+        return (getGalutinis() > a.getGalutinis());
+    }
+    bool operator ==(const Studentas& a) const {
+        if (getVardas() != a.getVardas())
+            return 0;
+
+        if (getPavarde() != a.getPavarde())
+            return 0;
+
+        if (getND() != a.getND())
+            return 0;
+
+        if (getNDskaicius() != a.getNDskaicius())
+            return 0;
+
+        if (getEgzaminas() != a.getEgzaminas())
+            return 0;
+
+        if (getGalutinis() != a.getGalutinis())
+            return 0;
+
+        if (getVidarmed() != a.getVidarmed())
+            return 0;
+
+        return 1;
+    }
+    void isvedimas(char separator, int VardSimb, int PavSimb, int GalutSimb) {
+        const char separator_ = separator;
+        const int VardSimb_ = VardSimb;
+        const int PavSimb_ = PavSimb;
+        const int GalutSimb_ = GalutSimb;
+        Zmogus::isvedimas(separator_, VardSimb_, PavSimb_);
+        cout << left << setw(GalutSimb_) << setfill(separator_) << fixed << setprecision(2) << getGalutinis() << endl;
+    }
 };
--- zmogus.h
+++ zmogus.h
@@ -26,14 +26,35 @@ protected:
         Vardas_ = vardas;
         Pavarde_ = pavarde;
     }
-    Zmogus(const Zmogus& zmg);
-    Zmogus& operator=(const Zmogus& zmg);
+    Zmogus(const Zmogus& zmg)
+        : Vardas_{ zmg.Vardas_ },
+        Pavarde_{ zmg.Pavarde_ } {
+        Vardas_ = zmg.Vardas_;
+        Pavarde_ = zmg.Pavarde_;
+    }
+    Zmogus& operator=(const Zmogus& zmg) {
+        if (&zmg == this) return *this;
+        Vardas_ = zmg.Vardas_;
+        Pavarde_ = zmg.Pavarde_;
+        return *this;
+    }
     void setVardas(string vardas) { Vardas_ = vardas; }
     void setPavarde(string pavarde) { Pavarde_ = pavarde; }
 public:
     virtual ~Zmogus() { }
     virtual string getVardas() const { return Vardas_; }
     virtual string getPavarde() const { return Pavarde_; }
-    virtual bool operator <(const Zmogus& a) const;
-    virtual void isvedimas(char separator, int VardSimb, int PavSimb);
+    virtual bool operator <(const Zmogus& a) const {
+        if (getVardas() != a.getVardas())
+            return getVardas().compare(a.getVardas()) < 0;
+
+        return getPavarde().compare(a.getPavarde()) < 0;
+    }
+    virtual void isvedimas(char separator, int VardSimb, int PavSimb) {
+        const char separator_ = separator;
+        const int VardSimb_ = VardSimb;
+        const int PavSimb_ = PavSimb;
+        cout << left << setw(VardSimb_) << setfill(separator_) << getVardas();
+        cout << left << setw(PavSimb_) << setfill(separator_) << getPavarde();
+    }
 };
